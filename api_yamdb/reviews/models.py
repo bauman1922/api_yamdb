d2b54b07f5606194bd393db501cd203b1b6b6cd4
unknown <stas.basov@whatever.com>
2023-05-24 00:34:48 +0300

    create model for Reviews and Comments

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from usrs.models import User
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField(verbose_name='Оценка произведения',
+                                validators=(MinValueValidator(1),
+                                            MaxValueValidator(10)))
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text()
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text()
