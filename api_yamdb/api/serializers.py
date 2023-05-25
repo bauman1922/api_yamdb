f136b7d419567f411d2a53b4bd72968a2a91601b
Сергей Саморуков <bauman1922@gmail.com>
2023-05-25 19:12:10 +0300

    Сериализаторы

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+import datetime as dt
 
 from reviews.models import Category, Genre, GenreTitle, Title
 
@@ -6,48 +7,54 @@ from reviews.models import Category, Genre, GenreTitle, Title
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ('name', 'slug')
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = '__all__'
+        fields = ('name', 'slug')
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    # user = serializers.SlugRelatedField(
-    #     slug_field='username',
-    #     read_only=True,
-    #     default=serializers.CurrentUserDefault()
-    # )
-    # following = serializers.SlugRelatedField(
-    #     slug_field='username',
-    #     queryset=User.objects.all()
-    # )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
 
     class Meta:
+        fields = (
+            'id', 'name', 'year', 'description', 'genre', 'category'
+        )
         model = Title
-        fields = '__all__'
 
-    #     validators = [
-    #         UniqueTogetherValidator(
-    #             queryset=Follow.objects.all(),
-    #             fields=('user', 'following')
-    #         )
-    #     ]
-
-    # def validate(self, data):
-    #     if self.context.get('request').user == data.get('following'):
-    #         raise serializers.ValidationError(
-    #             'Невозможно оформить подписку на самого себя!')
-    #     return data
+    def validate_year(self, value):
+        year = dt.date.today().year
+        if value > year:
+            raise serializers.ValidationError(
+                'Год выпуска не может быть больше текущего!')
+        return value
 
 
-class GenreTitleSerializer(serializers.ModelSerializer):
-    # author = serializers.SlugRelatedField(
-    #     slug_field='username', read_only=True)
+class TitleListSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(read_only=True, many=True)
 
     class Meta:
-        model = GenreTitle
-        fields = '__all__'
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+        model = Title
+
+
+
+# class GenreTitleSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = GenreTitle
+#         fields = '__all__'
