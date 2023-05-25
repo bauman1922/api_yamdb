16409793e8a512dd2712abb85e03b682f980af7c
Сергей Саморуков <bauman1922@gmail.com>
2023-05-25 15:12:48 +0300

    Сериализаторы, вьюсеты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, filters
+from rest_framework.pagination import LimitOffsetPagination
 
 from reviews.models import Category, Genre, Title
 
@@ -15,11 +16,17 @@ class SimpleViewSet(mixins.ListModelMixin,
 class CategoryViewSet(SimpleViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
 
 
 class GenreViewSet(SimpleViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
